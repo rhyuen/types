2c808a2e9c2a0a34f33ae75cdcd095300cd2f838
Robert Yuen <dungeonry@hotmail.com>
2019-09-07 16:59:40 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Some types for Golang due to lack of understanding of go modules.
---
+++ types.go
@@ -0,0 +1,6 @@
+package types
+
+type Quote struct {
+	Author string `json:"author"`
+	Text   string `json:"text"`
+}
